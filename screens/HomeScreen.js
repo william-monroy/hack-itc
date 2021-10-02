3aa574d79ac3dc2684e5576606160c754fea3ef0
william-monroy <william_monroy777@hotmail.com>
2021-10-02 15:21:50 -0500

    design app

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,15 @@ import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import tw from "tailwind-react-native-classnames";
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_KEY} from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = ({navigarion}) => {
+    
+    const dispatch = useDispatch();
+
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5 pt-10`}>
@@ -17,6 +24,36 @@ const HomeScreen = ({navigarion}) => {
                         uri: 'https://facebook.github.io/react-native/img/tiny_logo.png',
                     }}
                 />
+
+                <GooglePlacesAutocomplete
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput:{
+                            fontSize: 18,
+                        }
+                    }}
+                    query={{
+                        key: GOOGLE_MAPS_KEY,
+                        language: 'es',
+                    }}
+                    minLength={2}
+                    onPress={(data, details = null) => {
+                        dispatch(setOrigin({
+                            location: details.geometry.location,
+                            description: data.description
+                        }))
+                        dispatch(setDestination(null))
+                    }}
+                    fetchDetails={true}
+                    returnKeyType={'Search'}
+                    enablePoweredByContainer={false} 
+                    nearbyPlacesAPI='GooglePlacesSearch'
+                    debounce={400}
+                    placeholder="De dónde vamos?"
+                />
+
                 <NavOptions />
             </View>
         </SafeAreaView>
